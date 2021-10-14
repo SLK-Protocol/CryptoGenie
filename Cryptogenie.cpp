608f3f10343b87971825c193fd023779ae661c8d
Dragos Sabau <dragos.sabau@sincaibm.ro>
2021-10-14 18:36:49 +0300

    CryptoGenie : Reworked Algorithms
    Reworked the algoritms, added decode to all algorithms, Added encrypted remote communication.

--- Cryptogenie.cpp
+++ Cryptogenie.cpp
@@ -2,7 +2,28 @@
 #include <cstring>
 #include <string>
 #include <fstream>
+#include <cmath>
 using namespace std;
+void rs(int a, int &fin)
+{
+    int g=2,p=5,gl,gp;
+    cout<<"===================== REMOTE COMMUNICATION =====================";
+    cout<<'\n'<<'\n';
+    cout<<"Local=";
+    cin>>a;
+    cout<<'\n';
+    gl=pow(g,a);
+    gl=gl%p;
+    cout<<"Give to partner="<<gl;
+    cout<<'\n'<<'\n';
+    cout<<"Partner=";
+    cin>>gp;
+    cout<<'\n';
+    fin=pow(gp,a);
+    fin=fin%5;
+    cout<<"===================== CRYPTOGENIE BY DKD =======================";
+    cout<<'\n'<<'\n';
+}
 void encode1(char s[300000], char r[300000])
 {
     char cod;
@@ -162,27 +183,87 @@ void decode5 (char r[300000], char t[300000])
 int main()
 {
     char s[300000],r[300000],t[300000]="";
+    int men,alg,a,fin;
+    rs(a,fin);
+    cout<<"1-Encode 2-Decode"<<'\n';
+    cout<<'\n';
+    do
+    {
+        cout<<"Option: ";
+        cin>>men;
+    }
+    while(men<1 || men>2);
+    cout<<'\n';
     cout<<"Give text: ";
-    cin.get(s,300000);
-    encode1(s,r);
-    cout<<r<<'\n';
-   decode1(r,t);
-    cout<<t<<'\n';
-   encode2(s,r);
-    cout<<r<<'\n';
-    decode2(r,t);
-    cout<<t<<'\n';
- encode3(s,r);
-    cout<<r<<'\n';
-    decode3(r,t);
-    cout<<t<<'\n';
-     encode4(s,r);
-    cout<<r<<'\n';
-    decode4(r,t);
-    cout<<t<<'\n';
-     encode5(s,r);
-    cout<<r<<'\n';
-    decode5(r,t);
-    cout<<t<<'\n';
+    cin.get();
+    cin.getline(s,300000);
+    cout<<'\n';
+    if(men==1)
+    {
+        if(fin==1)
+        {
+            encode1(s,r);
+            cout<<"Encoded Text: "<<r;
+            cout<<'\n';
+        }
+        if(fin==2)
+        {
+            encode2(s,r);
+            cout<<"Encoded Text: "<<r;
+            cout<<'\n';
+        }
+        if(fin==3)
+        {
+            encode3(s,r);
+            cout<<"Encoded Text: "<<r;
+            cout<<'\n';
+        }
+        if(fin==4)
+        {
+            encode4(s,r);
+            cout<<"Encoded Text: "<<r;
+            cout<<'\n';
+        }
+        if(fin==5)
+        {
+            encode5(s,r);
+            cout<<"Encoded Text: "<<r;
+            cout<<'\n';
+        }
+    }
+    if(men==2)
+    {
+
+        if(fin==1)
+        {
+            decode1(s,t);
+            cout<<"Decoded Text: "<<t;
+            cout<<'\n';
+        }
+        if(fin==2)
+        {
+            decode2(s,t);
+            cout<<"Decoded Text: "<<t;
+            cout<<'\n';
+        }
+        if(fin==3)
+        {
+            decode3(s,t);
+            cout<<"Decoded Text: "<<t;
+            cout<<'\n';
+        }
+        if(fin==4)
+        {
+            decode4(s,t);
+            cout<<"Decoded Text: "<<t;
+            cout<<'\n';
+        }
+        if(fin==5)
+        {
+            decode5(s,t);
+            cout<<"Decoded Text: "<<t;
+            cout<<'\n';
+        }
+    }
     return 0;
 }
