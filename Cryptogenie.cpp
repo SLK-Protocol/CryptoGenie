b9c8139f19bdaa9e04aad0f1fef661ef69235048
cryptolazea <lazeadarius57@gmail.com>
2021-10-14 15:10:48 +0300

    CryptoGenie : Automatized finding of the inverse of a at every decription. Added decription 2-5.
    cateva modificari, C++ afla acuma inversa lui a automat nu mai trebe sa fac eu de mana, adaugat decriptii 2-5. Par sa mearga si la texte lungi, putine teste in zona asta  ar fi pontix.

--- Cryptogenie.cpp
+++ Cryptogenie.cpp
@@ -17,15 +17,17 @@ void encode1(char s[300000], char r[300000])
 void decode1(char r[300000], char t[300000])
 {
     char cod;
-    int i,a=2,b=3,x,j;
+    int i,a=2,b=3,x,j,simetric=1;
     strcpy(t,r);
+    while ((a*simetric)%223 != 1)
+        simetric++;
     for(i=0; i<strlen(r); i++)
     {
         j=i%223;
         x=int (r[i]);
         while (x<0)
             x=x+256;
-        x=(x-32-b-j*j*j)*112;
+        x=(x-32-b-j*j*j)*simetric;
         while (x<0)
             x=x+223;
         cod=char(x%223+32);
@@ -43,10 +45,30 @@ void encode2(char s[300000], char r[300000])
         r[i]=cod;
     }
 }
+void decode2 (char r[300000], char t[300000])
+{
+    char cod;
+    int i,a=65,b=18,x,j,simetric=1;
+    strcpy(t,r);
+    while ((a*simetric)%223 != 1)
+        simetric++;
+    for(i=0; i<strlen(r); i++)
+    {
+        j=i%223;
+        x=int (r[i]);
+        while (x<0)
+            x=x+256;
+        x=(x-32-b-j*j*j)*simetric;
+        while (x<0)
+            x=x+223;
+        cod=char(x%223+32);
+        t[i]=cod;
+    }
+}
 void encode3(char s[300000], char r[300000])
 {
     char cod;
-    int i,a=2,b=3;
+    int i,a=8,b=8;
     strcpy(r,s);
     for(i=0; i<strlen(s); i++)
     {
@@ -54,26 +76,87 @@ void encode3(char s[300000], char r[300000])
         r[i]=cod;
     }
 }
+void decode3(char r[300000], char t[300000])
+{
+    char cod;
+    int i,a=8,b=8,x,j,simetric=1;
+    strcpy(t,r);
+    while ((a*simetric)%223 != 1)
+        simetric++;
+    for(i=0; i<strlen(r); i++)
+    {
+        j=i%223;
+        x=int (r[i]);
+        while (x<0)
+            x=x+256;
+        x=(x-32-b-j*j*j)*simetric;
+        while (x<0)
+            x=x+223;
+        cod=char(x%223+32);
+        t[i]=cod;
+    }
+}
+
 void encode4(char s[300000], char r[300000])
 {
     char cod;
-    int i,a=2,b=3;
+    int i,a=9,b=9;
     strcpy(r,s);
     for(i=0; i<strlen(s); i++)
     {
         cod=char(((int(s[i])-32)*a+b+i*i*i)%223+32);
         r[i]=cod;
+    }
+}
+void decode4(char r[300000], char t[300000])
+{
+    char cod;
+    int i,a=9,b=9,x,j,simetric=1;
+    strcpy(t,r);
+    while ((a*simetric)%223 != 1)
+        simetric++;
+    for(i=0; i<strlen(r); i++)
+    {
+        j=i%223;
+        x=int (r[i]);
+        while (x<0)
+            x=x+256;
+        x=(x-32-b-j*j*j)*simetric;
+        while (x<0)
+            x=x+223;
+        cod=char(x%223+32);
+        t[i]=cod;
     }
 }
 void encode5(char s[300000], char r[300000])
 {
     char cod;
-    int i,a=2,b=3;
+    int i,a=2,b=7;
     strcpy(r,s);
     for(i=0; i<strlen(s); i++)
     {
         cod=char(((int(s[i])-32)*a+b+i*i*i)%223+32);
         r[i]=cod;
+    }
+}
+void decode5 (char r[300000], char t[300000])
+{
+    char cod;
+    int i,a=2,b=7,x,j,simetric=1;
+    strcpy(t,r);
+    while ((a*simetric)%223 != 1)
+        simetric++;
+    for(i=0; i<strlen(r); i++)
+    {
+        j=i%223;
+        x=int (r[i]);
+        while (x<0)
+            x=x+256;
+        x=(x-32-b-j*j*j)*simetric;
+        while (x<0)
+            x=x+223;
+        cod=char(x%223+32);
+        t[i]=cod;
     }
 }
 int main()
@@ -83,8 +166,23 @@ int main()
     cin.get(s,300000);
     encode1(s,r);
     cout<<r<<'\n';
-    decode1(r,t);
-    cout<<t;
-
+   decode1(r,t);
+    cout<<t<<'\n';
+   encode2(s,r);
+    cout<<r<<'\n';
+    decode2(r,t);
+    cout<<t<<'\n';
+ encode3(s,r);
+    cout<<r<<'\n';
+    decode3(r,t);
+    cout<<t<<'\n';
+     encode4(s,r);
+    cout<<r<<'\n';
+    decode4(r,t);
+    cout<<t<<'\n';
+     encode5(s,r);
+    cout<<r<<'\n';
+    decode5(r,t);
+    cout<<t<<'\n';
     return 0;
 }
