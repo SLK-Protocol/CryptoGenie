a54ab8e73562427d2f2fab8f1d9af76c290869ff
Dragos Sabau <dragos.sabau@sincaibm.ro>
2021-10-17 18:08:05 +0300

    CryptoGenie : Indentation fix & Bug Fix
    Fixed Indentation and removed unnecessary lines . Fixed bug where give text line appeared before new key generation.

--- Cryptogenie.cpp
+++ Cryptogenie.cpp
@@ -5,7 +5,6 @@
 #include <cmath>
 using namespace std;
 int v[110],j,u[101];
-
 void encode1(char s[300000], char r[300000])
 {
     char cod;
@@ -13,7 +12,6 @@ void encode1(char s[300000], char r[300000])
     strcpy(r,s);
     for(i=0; i<strlen(s); i++)
     {
-
         cod=char(((int(s[i])-31)*u[i%80])%223+31);
         r[i]=cod;
     }
@@ -25,8 +23,9 @@ void decode1(char r[300000], char t[300000])
     strcpy (t,r);
 
     for(i=0; i<strlen(r); i++)
-    {   while ((u[i%80]*simetric)%223 != 1)
-        simetric++;
+    {
+        while((u[i%80]*simetric)%223!=1)
+            simetric++;
         x=int(r[i]);
         while (x<0)
             x=x+256;
@@ -106,55 +105,60 @@ int main()
             cin>>men;
         }
         while(men<1 || men>3);
-        cout<<'\n';
-        cout<<"Give text: ";
-        cin.get();
-        cin.getline(s,300000);
-        cout<<'\n';
         if(men==1)
         {
+            cout<<'\n';
+            cout<<"Give text: ";
+            cin.get();
+            cin.getline(s,300000);
+            cout<<'\n';
             cout<<"Encoded Text: ";
             encode1(s,r);
             cout<<r;
         }
         if (men==2)
         {
+            cout<<'\n';
+            cout<<"Give text: ";
+            cin.get();
+            cin.getline(s,300000);
+            cout<<'\n';
             cout<<"Decoded Text: ";
             decode1(s,t);
             cout<<t;
         }
-        if (men == 3)
+        if (men==3)
         {
-            for (k = 0; k <= 100; k++)
-                u[k] = v[k] * 10 + v[(k + 1)] + 2;
-            for (j = 0; j < 10; j++)
+            for(k=0; k<=100; k++)
+                u[k]=v[k]*10+v[(k+1)]+2;
+            for (j=0; j<10; j++)
             {
-                rs(a, fin);
+                rs(a,fin);
             }
-
-            cout << "1-Encode 2-Decode";
-            cout << '\n' << '\n';
+            cout<<"1-Encode 2-Decode";
+            cout<<'\n'<<'\n';
             do
             {
-                cout << "Option: ";
-                cin >> men;
-            } while (men < 1 || men>2);
-            cout << '\n';
-            cout << "Give text: ";
+                cout<<"Option: ";
+                cin>>men;
+            }
+            while(men<1 || men>2);
+            cout<<'\n';
+            cout<<"Give text: ";
             cin.get();
             cin.getline(s, 300000);
-            cout << '\n';
-            if (men == 1)
+            cout<<'\n';
+            if(men==1)
             {
-                cout << "Encoded Text: ";
+                cout<<"Encoded Text: ";
                 encode1(s, r);
-                cout << r;
+                cout<<r;
             }
-            if (men == 2)
+            if (men==2)
             {
-                cout << "Decoded Text: ";
+                cout<<"Decoded Text: ";
                 decode1(s, t);
-                cout << t;
+                cout<<t;
             }
         }
         cout<<'\n'<<'\n';
