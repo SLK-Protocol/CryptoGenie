9ba616fe15be7f5dcb63e06779c0baf6b8152d94
Dragos Sabau <dragos.sabau@sincaibm.ro>
2021-10-18 15:16:33 +0300

    CryptoGenie : Fixed New Key Generation loop

--- Cryptogenie.cpp
+++ Cryptogenie.cpp
@@ -97,7 +97,7 @@ int main()
         for(k=0; k<=100; k++)
             u[k]=v[k]*10+v[(k+1)]+2;
 
-        cout<<"1-Encode 2-Decode 3-Generate a new key";
+        cout<<"1-Encode 2-Decode 3-New Key";
         cout<<'\n'<<'\n';
         do
         {
@@ -129,37 +129,7 @@ int main()
         }
         if (men==3)
         {
-            for(k=0; k<=100; k++)
-                u[k]=v[k]*10+v[(k+1)]+2;
-            for (j=0; j<10; j++)
-            {
-                rs(a,fin);
-            }
-            cout<<"1-Encode 2-Decode";
-            cout<<'\n'<<'\n';
-            do
-            {
-                cout<<"Option: ";
-                cin>>men;
-            }
-            while(men<1 || men>2);
-            cout<<'\n';
-            cout<<"Give text: ";
-            cin.get();
-            cin.getline(s, 300000);
-            cout<<'\n';
-            if(men==1)
-            {
-                cout<<"Encoded Text: ";
-                encode1(s, r);
-                cout<<r;
-            }
-            if (men==2)
-            {
-                cout<<"Decoded Text: ";
-                decode1(s, t);
-                cout<<t;
-            }
+            main();
         }
         cout<<'\n'<<'\n';
         cout<<"======================== SLK-PROTOCOL ==========================";
