43820f6e1c08d0f3a8c12e608619048d0a5edf91
cryptolazea <lazeadarius57@gmail.com>
2021-10-26 14:34:47 +0300

    Cryptogenie : Testing medium
    Did some tests to identify and solve some problems

--- Cryptogenie.cpp
+++ Cryptogenie.cpp
@@ -5,7 +5,8 @@
 
 using namespace std;
 int n;
-int a[100][100],v[100];
+int a[100][100],b[100][100],c[100][100],d[100][100],v[100],t[81],o;
+
 int impartire (int a, int b)
 {
     int x=1;
@@ -18,14 +19,26 @@ int impartire (int a, int b)
 }
 void inversa(int a[100][100])
 {
-    int i,j,y,k,h;
+    int i,j,y,k,h,S,q;
+    for(i=0;i<=80;i++)
+{
+    t[i]=i+1;
+}
     for(i=1; i<=n; i++)
     {
         for(j=1; j<=n; j++)
         {
-            cout<<"a["<< i<<"]"<< j<<"]= ";
-            cin>>a[i][j];
+           a[i][j]=(i+j-1)%n;
         }
+    }
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=n; j++)
+        {
+           b[i][j]=a[i][j];
+           cout<<b[i][j]<<" ";
+        }
+        cout<<endl;
     }
     for(i=1; i<=n; i++)
     {
@@ -45,8 +58,17 @@ void inversa(int a[100][100])
     {
         if(a[i][i] == 0)
         {
-            cout<<"Mathematical Error!";
-            exit(0);
+            cout<<"Mathematical Error!"<<i<<endl;
+            for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=n; j++)
+        {
+
+           cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+           exit(0);
         }
         for(j=i+1; j<=n; j++)
         {
@@ -81,15 +103,43 @@ void inversa(int a[100][100])
     }
     for(i=1; i<=n; i++)
     {
+        for(j=1; j<=n; j++)
+        {
+            c[i][j]=a[i][j+n];
+            cout<<c[i][j]<<" ";
+        }
+        cout<<endl;
+    } for(i=1; i<=n; i++)
+    {
         for(j=1; j<=2*n; j++)
         {
             cout<<a[i][j]<<" ";
         }
+    }
+    cout<<endl;
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=n; j++)
+        {
+            d[i][j]=0;
+            for(k=1;k<=n;k++)
+                d[i][j]=(d[i][j]+b[i][k]*c[k][j])%223;
+
+        }
+    }
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=n; j++)
+        {
+            cout<<d[i][j]<<" ";
+        }
+        cout<<endl;
     }
 }
     int main ()
     {
         cin>>n;
         inversa (a);
+
         return 0;
     }
